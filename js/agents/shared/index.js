5dfd4df346088fc9d4fa471259f1cc95742a44e8
wing <wwing7332@gmail.com>
2026-01-02 14:20:50 +0800

    refactor(M-008): export shared utilities

--- js/agents/shared/index.js
+++ js/agents/shared/index.js
@@ -13,6 +13,9 @@ export { robustParseJson } from "./utils/robust-json.js";
 export { createStageApi } from "./utils/stage-api.js";
 export { isPlainObject, toNonEmptyString } from "./utils/value-utils.js";
 export { createLogger, trackToolCall } from "./utils/logger.js";
+export { Deque } from "./utils/deque.js";
+export { safeJsonParse } from "./utils/safe-json.js";
+export { extractJsonCandidate, stripThinkingTags } from "./utils/json-candidate.js";
 
 // Storage & Resilience
 export {
@@ -33,3 +36,7 @@ export {
   getCircuitBreaker,
   withCircuitBreaker,
 } from "./utils/circuit-breaker.js";
+
+// Embeddings
+export { EmbeddingService, createEmbeddingService, normalizeEmbeddingConfig } from "./embeddings/embedding-service.js";
+export { VectorIndex } from "./embeddings/vector-index.js";
