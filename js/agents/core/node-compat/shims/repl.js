f40ef760f200eaed99b29d3a3857941217a1a17d
wing <wwing7332@gmail.com>
2026-02-13 09:07:22 +0800

    Revert "feat(node-compat): 提取 console/constants/repl/sys 为独立 shim，达到 50 个"
    This reverts commit bf5ad1241d7ae488e588852de6f1902d532f3ee2.

--- js/agents/core/node-compat/shims/repl.js
+++
@@ -1,9 +0,0 @@
-/**
- * repl shim - REPL module stub
- * Not supported in browser environment
- */
-
-const noop = () => {};
-const noopStub = new Proxy({}, { get: () => noop });
-
-export default noopStub;
